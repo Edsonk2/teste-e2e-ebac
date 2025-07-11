4804c7fd3a813064ae5ed6e3edcad2c99f5e2752
edsonoliveira <edsonbrasiloliveira3@gmail.com>
2025-07-10 21:14:18 -0300

    exercicío de e2e

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -10,11 +10,31 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+      cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
       //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
+
+      cy.contains('Abominable Hoodie').click()
+      cy.get('.button-variable-item-L').click()
+      cy.get('.button-variable-item-Green').click()
+      cy.get('.input-text').clear().type(1)
+      cy.get('.single_add_to_cart_button').click()
+        
+    
+    cy.contains('Aero Daily Fitness Tee').click()
+
+
+
+
+
+
+
+
+
+
+
       
   });
 
